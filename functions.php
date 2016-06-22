05512d89fc33d077e9beefcab623809d4835316a
Maxime BERNARD-JACQUET <maxime@smoothie-creative.com>
2016-06-22 08:59:52 +0200

    Autoload class and added execute function

--- functions.php
+++ functions.php
@@ -1,84 +1,39 @@
 <?php
 
-// Thumbnails, Scripts, admin cleanup, site optimization...
-// include('core/theme-config.php');
+define('MAINTENANCE', false);
+define('DISALLOW_FILE_EDIT', true);
 
-// // CPT
-// include('core/cpt.php');
-
-// // ACF Options pages
-// include('core/acf.php');
-
-// // Filters, Templates helpers...
-// include('core/theme-features.php');
-
-// // Ajax
-// include('core/ajax.php');
-
-// // Plugins
-// include('core/plugins.php');
-
-// WP API mods
-//include('core/wp-api.php');
-
-
-class Dysign_Theme() {
-
-  private const $maintenance = false;
-
-  public function __construct() {
-
-    if($this->$maintenance)
-      add_action('get_header', array($this, 'activate_maintenance'));
-
-    add_action( 'after_setup_theme', array($this, 'theme_setup'));
+function __autoload($class_name) { 
+  if(strpos($class_name, 'Dysign_') !== false){
+    $class_name = strtolower(str_replace('_', '-', $class_name));
+    include get_template_directory().'/core/class-'.$class_name.'.php';
   }
+} 
 
-  private function theme_setup() {
+// Hooks
+$dysign_theme_hooks = new Dysign_Theme_Hooks();
+$dysign_theme_hooks->execute();
 
-    // Text Domain
-    load_theme_textdomain('dysign', get_template_directory() . '/languages');
+// Post Types
+$dysign_theme_cpt = new Dysign_Theme_CPT();
+$dysign_theme_cpt->execute();
 
-    //  Thumbnails
-    add_theme_support('post-thumbnails');
-    set_post_thumbnail_size(380, 230, true );
-    //add_image_size('slider', 540, 350, true);
+// Theme features
+$dysign_theme_features = new Dysign_Theme_Features();
+$dysign_theme_features->execute();
 
-    //  Page Title
-    add_theme_support('title-tag');
+// ACF
+$dysign_theme_acf = new Dysign_Theme_ACF();
+$dysign_theme_acf->execute();
 
-    // Menus
-    register_nav_menus( array(
-      'main' => 'Menu Principal',
-      'footer' => 'Pied de page'
-    ));
+// Ajax
+$dysign_theme_ajax = new Dysign_Theme_Ajax();
+$dysign_theme_ajax->execute();
 
-    // Editor Tiny MCE custom styles
-    add_editor_style(array('css/editor-style.css'));
+// Plugins
+$dysign_theme_plugins = new Dysign_Theme_Plugins();
+$dysign_theme_plugins->execute();
 
-    // Enable HTML5
-    add_theme_support('html5', array(
-      'search-form',
-      'comment-form',
-      'comment-list',
-      'gallery',
-      'caption',
-    ));
-
-    // RSS
-    add_theme_support('automatic-feed-links');
-
-    // Remove post format
-    remove_theme_support('post-formats');
-  }
-
-
-  private function activate_maintenance() {
-    if ( !current_user_can('edit_themes') || !is_user_logged_in() ) {
-      wp_die('Site en maintenance, merci de revenir dans un moment.');
-    }
-  }
-
-}
-
-$dysign_theme = new Dysign_Theme();
+// WP API
+$dysign_theme_api = new Dysign_Theme_API();
+$dysign_theme_api->execute();
