d0565f9ca1732f50e280279eb3da27b41f9d16a9
Maxime BERNARD-JACQUET <maxime@smoothie-creative.com>
2017-09-18 18:27:42 +0200

    Default markup in template file

--- templates/page.php
+++ templates/page.php
@@ -2,4 +2,19 @@
 /*
   Template Name: Nom du template
 */
+  
+  get_header();
+  if ( have_posts() ) : while ( have_posts() ) : the_post();
+?>
+
+  <h1><?php the_title(); ?></h1>
+
+  <div class="content">
+    <?php the_content(); ?>
+  </div>
+
+<?php
+  endwhile;
+  endif;
+  get_footer();
 ?>
